f2946578fa4a2c308635f41433f9e96be154add8
prousitbat <jennifer.proust@gmail.com>
2024-12-23 21:46:04 +0100

    fix(loader): prevent broken path when building

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,13 +1,17 @@
 import { ReactElement } from "react";
 import { API_TYPE } from "../apis/the-cat.ts";
-
-let source: string;
-import( `../assets/${ API_TYPE === "dogs" ? "dog" : "cat" }.gif` ).then( ( module ) => {
-    source = module.default;
-} );
+import dogGif from "../assets/dog.gif";
+import catGif from "../assets/cat.gif";
 
 const Loader = (): ReactElement => {
-    return <img width="64" height="64" src={source} alt="loading..." />;
+    return <img
+        width="64"
+        height="64"
+        src={API_TYPE === "dogs"
+            ? dogGif
+            : catGif}
+        alt="loading..."
+    />;
 };
 
 export default Loader;
