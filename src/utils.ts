291a87c3b4a5e26bda5458c33a561d74abd529b5
prousitbat <jennifer.proust@gmail.com>
2024-12-25 17:45:48 +0100

    feat(uuid): add lib to prevent error on local devices with crypto.randomUUID

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,4 @@
+import { v4 as uuid } from "uuid";
+
 export const getAverage = ( arr: number[] ) => ( arr.reduce( ( acc, curr ) => acc + curr, 0 ) ) / arr.length;
+export const getUid = (): string => crypto.randomUUID ? crypto.randomUUID() : uuid();
